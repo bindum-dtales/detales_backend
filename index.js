cd580b64da7b7bb28e785dbbf86241baa77213c3
siddhanth36 <sksiddhanth69@gmail.com>
2026-02-22 16:10:51 +0530

    fix the portfolio v11

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 
+import portfolioRoute from "./routes/portfolio.js";
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -9,8 +11,10 @@ app.use(cors({ origin: true }));
 app.use(express.json({ limit: "10mb" }));
 
 app.get("/", (req, res) => {
-  res.send("SERVER RUNNING - STEP 1");
+  res.send("SERVER RUNNING - STEP 2");
 });
+
+app.use("/api/portfolio", portfolioRoute);
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("SERVER STARTED ON PORT", PORT);
